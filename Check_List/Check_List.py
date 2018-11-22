f6370c9bd8754073551acab74111a406222cee25
Alix Janicaud <alix.janicaud@free.fr>
2018-11-22 17:43:26 +0100

    finish check list display on interface

--- Check_List/Check_List.py
+++ Check_List/Check_List.py
@@ -10,21 +10,20 @@ Created on Mon Oct 29 19:04:28 2018
 # Static model
 # ________________________________________________________________________________
 #
-# Databases
-
+# Databases retrieval and displaying
+#
 # ________________________________________________________________________________
 
 
 # Importation
 import xlrd
-#import pandas as pd
-
 from tkinter import *
 from functools import partial
 import tkinter.font as tkFont
 
-#from tkinter.ttk import Combobox
-
+# ________________________________________________________________________________
+#
+# Defines the data not accessible yet
 # ________________________________________________________________________________
 
 # Aicraft data - not contained in the databases
@@ -36,7 +35,13 @@ course = 2 # in XX
 altitude = 2 # in XX
 gaz = 2 # in XX - gaz remaining in the airplane
 
+
+
 # ________________________________________________________________________________
+#
+# Gets the databases from excel files
+# ________________________________________________________________________________
+
 
 # Import the excel files containing the databases
 check_lists = xlrd.open_workbook('../Databases/Check_lists_DR400.xlsx') # Flight Check lists
@@ -48,40 +53,54 @@ airfields_database = xlrd.open_workbook('../Databases/airfields_database.xlsx') 
 names_cl = check_lists.sheet_names()
 
 
+# ________________________________________________________________________________
+#
+# Displays the tasks on the interface for the pilote to see
+# ________________________________________________________________________________
+
 
 """
 Function : next_window
            Allows the interface to switch to the next task when the current task is completed
-@param : int i
+           Is called by hitting the ok-button
+@param : IntVar counter
+         int n
+         tkinter.Tk Check_list
+         Labels prev_task, current_task, action and next_task
+         xlrd.sheet.Sheet current_cl
 @return : void
-        prints on the interface
+        increments the counter of one
+        displays the next task in the check list
 """
-def my_func(compteur,n, prev_task, current_task, comment, next_task, current_cl):
-
-     if (compteur.get()<n):
-         compteur.set(compteur.get()+1)
-         i = int(compteur.get())
-         if (i == 2) :
-             prev_task.set('')
-         else :
-             prev_task.set(current_cl.col_values(2)[i-1])
+def next_window(counter,n, Check_list, prev_task, current_task, action, next_task, current_cl):
+    counter.set(counter.get()+1) # Increments the counter of one
+    i = int(counter.get()) # Transforms the counter from an IntVar to and int 
+    # Modify tasks displayed
+    if (counter.get()<n-1):
+         prev_task.set(current_cl.col_values(2)[i-1])
          current_task.set(current_cl.col_values(2)[i])
-         comment.set(current_cl.col_values(3)[i])
+         action.set(current_cl.col_values(3)[i])
          next_task.set(current_cl.col_values(2)[i+1])
-     else :
-         Check_list.destroy()
+    elif (counter.get() == n-1):
+         prev_task.set(current_cl.col_values(2)[i-1])
+         current_task.set(current_cl.col_values(2)[i])
+         action.set(current_cl.col_values(3)[i])
+         next_task.set('') # When the last task has been reached, there is nothing else to do
+    else :
+         Check_list.destroy()  # Stop when last task has been displayed
 
-# ________________________________________________________________________________
+
 """
 Function : print_check_list
-        prints the tasks of the check list on the interface
+           prints the tasks of the check list on the interface
 @param : string - current_check_list
-        name of check list considered
-        is an element of names_cl
+         name of check list considered
+         is an element of names_cl
 @return : void
         prints on the interface
 """
 def print_check_list(current_check_list) :
+    
     # The check list considered must be defined in the database
     if (current_check_list not in names_cl): 
         return "Error" 
@@ -92,8 +111,11 @@ def print_check_list(current_check_list) :
     # Name of the checklist
     title = current_cl.col_values(0)[0]
     
-    # Number of tasks in the checklist
-    n=5
+    # Number of lines in the checklist
+    n = current_cl.nrows
+    
+    #===============================================================================
+    # Settings of the window
     
     #Opening of the first interface
     Check_list = Tk()
@@ -108,11 +130,7 @@ def print_check_list(current_check_list) :
     Title1 = Label(Check_list, text=title, pady=3, height=2, font=font1, fg='red')
     Title1.pack()
     
-
-    #===============================================================================
-    #We set all the names and the position of all the Labelframe (small frame)
-    
-    #Prevous task
+    #Previous task
     prev_task_frame = LabelFrame(Check_list, height=60, text="Tâche précédente")
     prev_task_frame.grid_propagate(0)
     prev_task_frame.pack(fill="both", expand="no", padx=100)
@@ -126,79 +144,40 @@ def print_check_list(current_check_list) :
     next_task_frame = LabelFrame(Check_list, height=60, text="Tâche suivante")
     next_task_frame.grid_propagate(0)
     next_task_frame.pack(fill="both", expand="no", padx=100)
-    
 
 
+    #===============================================================================
+    # Content of the frames
 
-#===============================================================================
-
-    compteur = IntVar(value=1)
-    i = int(compteur.get())
+    counter = IntVar(value=2) # Counter that will keep track of the current task
+    i = int(counter.get()) # Conversion of this counter to int to allow its use as list index
     
-    prev_task = StringVar(value = '')
-    current_task = StringVar(value = '')#current_cl.col_values(2)[i])
-    comment = StringVar(value = '')#current_cl.col_values(3)[i])
-    next_task = StringVar(value = '')#current_cl.col_values(2)[i+1])
+    # The content of the first window to be displayed
+    prev_task = StringVar(value = '') 
+    current_task = StringVar(value = current_cl.col_values(2)[i])
+    action = StringVar(value = current_cl.col_values(3)[i])
+    next_task = StringVar(value = current_cl.col_values(2)[i+1])
     
-    
-    # Previous task
+    # All the labels that will be displayed on the interface
     prev_task_label = Label(prev_task_frame, textvariable = prev_task, font=font2).place(x=30, y=5)
-
-    
-    #Current Task
     current_task_label = Label(current_task_frame, textvariable = current_task, font=font2).place(x=30, y=5)
     to_do_label = Label(current_task_frame, text="To do", font=font2).place(x=30, y=40)
-    comment_label = Label(current_task_frame, textvariable = comment, font=font2).place(x=100, y=60)
-    
-    #Next task
+    action_label = Label(current_task_frame, textvariable = action, font=font2).place(x=100, y=60)
     next_task_label = Label(next_task_frame, textvariable = next_task, font=font2).place(x=30, y=5)
       
-    
-    # When validation button hit, erase all, i = i +1
-    Validate = Button(Check_list, text = '  OK  ', fg='green', command=lambda :my_func(compteur,n, prev_task, current_task, comment, next_task, current_cl)).place(x=350, y=363)
+    #===============================================================================
+    # Validation button :
+        # is hit when current task is completed
+        # calls the next_window function
+    Validate = Button(Check_list, text = '  OK  ', fg='green', command=lambda :next_window(counter,n, Check_list, prev_task, current_task, action, next_task, current_cl)).place(x=350, y=363)
     
         
-        
+    # Runs the tkinter interface
     Check_list.mainloop()
- # ________________________________________________________________________________   
+  
     
   
-print_check_list('Visite_Prevol_Cabine')
+print_check_list('Visite_Prevol_Cabine') # Test to see if functions work
 
 
-
-
-
-
-
-
-# ________________________________________________________________________________
-"""
-Function : get_check_list
-        gets the elements of the check list, ready to be printed
-@param : string - current_check_list
-        name of check list considered
-        is an element of names_cl
-@return : 
-       number : list of int 
-       priority : list of int
-       description : list of string
-       to_do : list of string
-       comments : list of string
-"""
-#def get_check_list(current_check_list) :
-#    # The check list considered must be defined in the database
-#    if (current_check_list not in names_cl): 
-#        return "Error" 
-#    
-#    # We go in the correct tab
-#    current_cl = check_lists.sheet_by_name(names_cl == current_check_list)
-#    
-#    number = current_cl[:,0]  # order of the task
-#    priority = current_cl[:,1] #importance of the task
-#    description = current_cl[:,2] # description of the task
-#    to_do = current_cl[:,3] # action to realize
-#    comments = current_cl[:,4] # optional comments on the task
-#
-#    return number, priority, description, to_do, comments
-# ________________________________________________________________________________
+ # ________________________________________________________________________________ 
